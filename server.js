fb534ace7bdd765d12f2c76bb524a644009fa1f3
JunqiDu <50341989+JunqiDu@users.noreply.github.com>
2023-02-15 09:51:27 -0500

    Update UI of login&register
     - update UI
     - now nav logo can go back to index page

--- server.js
+++ server.js
@@ -76,6 +76,11 @@ app.get("/register", (req, res) => {
   res.render('register');
 });
 
+//click 'Sushi Time' on nav to go back to home page
+app.get("/index", (req, res) => {
+  res.render('index');
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
