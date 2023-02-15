c9347b686a69789b8f3f323dbc9b95b50f9973aa
Corey Silver <coreysilver@outlook.com>
2023-02-15 15:01:44 -0500

    Moved menu route to separate file

---
+++ routes/menu-router.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const cookieSession = require('cookie-session');
+const { getUserById } = require("../db/queries/userHelpers");
+
+router.get("/", (req, res) => {
+  getUserById(req.session["user_id"])
+  .then((response) => {
+    res.render("menu", {
+      user: req.session["user_id"],
+      userInfo: response
+    });
+  });
+});
+
+
+module.exports = router;
