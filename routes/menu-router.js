07adaebbf91439105a4bb6c85fc1253864086981
Corey Silver <coreysilver@outlook.com>
2023-02-16 16:49:55 -0500

    Fixed issue with logo not directing to home page. Also updated checkout-router to send text message.

--- routes/menu-router.js
+++ routes/menu-router.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const cookieSession = require("cookie-session");
 const { getUserById } = require("../db/queries/userHelpers");
 
+
+
 router.get("/", (req, res) => {
+  if (!req.session["user_id"]) {
+    res.redirect("login");
+  }
   getUserById(req.session["user_id"]).then((response) => {
     res.render("menu", {
       user: req.session["user_id"],
