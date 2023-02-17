a75fcc3eec15ad9c02a89b11c2d3b5ab5f576454
Corey Silver <coreysilver@outlook.com>
2023-02-17 00:54:02 -0500

    Fixed spelling errors on front end

--- db/seeds/03_items.sql
+++ db/seeds/03_items.sql
@@ -1,9 +1,9 @@
 INSERT INTO items (item_name, price, item_category, item_img)
 VALUES
 ('Miso Soup', 5, 'Appetizer', '/img/miso-soup.jpg'),
-('Onigiri (Japenese Riceballs)', 7, 'Appetizer', '/img/onigiri.jpg'),
-('Japenese Chilled Tofu', 8, 'Appetizer', '/img/chilled_tofu.jpg'),
-('Salted Adamame', 8, 'Appetizer', '/img/edamame.jpg'),
+('Onigiri (Japanese Riceballs)', 7, 'Appetizer', '/img/onigiri.jpg'),
+('Japanese Chilled Tofu', 8, 'Appetizer', '/img/chilled_tofu.jpg'),
+('Salted Adamame', 8, 'Appetizer', '/img/adamame.jpg'),
 ('California Rolls', 15, 'Main', '/img/california_rolls.jpg'),
 ('Chicken Teriyaki', 20, 'Main', '/img/chicken_teriyaki.jpg'),
 ('Shrimp Tempura', 16, 'Main', '/img/shrimp_tempura.jpg'),
@@ -11,4 +11,4 @@ VALUES
 ('Ice Cream', 5, 'Dessert', '/img/ice_cream.jpg'),
 ('Dorayaki', 8, 'Dessert', '/img/dorayaki.jpg'),
 ('Souffle Pancakes', 7, 'Dessert', '/img/souffle_pancakes.jpg'),
-('Japenese Cheesecake', 11, 'Dessert', '/img/cheesecake.jpg');
+('Japanese Cheesecake', 11, 'Dessert', '/img/cheesecake.jpg');
